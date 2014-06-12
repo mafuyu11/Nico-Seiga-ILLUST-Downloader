3cfbf4d770c5f730493bc63b7985e815ed7ab420
mafuyu11 <mafuyu11@naver.com>
2014-06-13 00:01:27 +0900

    ff

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Forms;
+
+namespace nocoSaiga
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ev_btn();
+        }
+
+        private XmlDataProvider _Prov { get { return (XmlDataProvider)Resources["newXML"]; } }
+
+        private void ev_btn()
+        {
+            PNewBtn.Click += (s, e) =>
+                {
+                    if (PSel.SelectedIndex > 0)
+                    {
+                        PSel.SelectedIndex = 0;
+                        _Prov.Source = null;
+                    }
+                    var ge = new getImg();
+                    ge.new_illust();
+                    _Prov.Source = new Uri(getImg.newXMLFile);
+                };
+
+            PSel.SelectionChanged += (s, e) =>
+                {
+                    if (PSel.SelectedIndex > 0)
+                    {
+                        _Prov.Source = null;
+                    }
+                    var sel = PSel.SelectedValue.ToString();
+                    sel = Regex.Split(sel, ": ")[1];
+                    var ge = new getImg();
+                    if (sel != "선택")
+                    {
+                        switch (sel)
+                        {
+                            case "러브라이브!":
+                                ge.sel_illust("ラブライブ！");
+                                _Prov.Source = new Uri(getImg.newXMLFile);
+                                break;
+                            case "칸코레":
+                                ge.sel_illust("艦これ");
+                                _Prov.Source = new Uri(getImg.newXMLFile);
+                                break;
+                            case "창작":
+                                ge.sel_illust("オリジナル");
+                                _Prov.Source = new Uri(getImg.newXMLFile);
+                                break;
+                            case "선택":
+                                _Prov.Source = null;
+                                break;
+                        }
+                    }
+                };
+  
+            PNew.MouseDoubleClick += (s, e) =>
+                {
+                    var imgSrc = PNew.SelectedValue.ToString();
+                    imgSrc = Regex.Split(imgSrc, "http://")[1];
+                    imgSrc = Regex.Split(imgSrc, "m출처")[0];
+                    var author = PNew.SelectedValue.ToString();
+                    author = Regex.Split(author, "http://")[0];
+                    author = Regex.Split(author, " : ")[1];
+                    var url = PNew.SelectedValue.ToString();
+                    url = Regex.Split(url, "출처 : ")[1];
+                    var res = System.Windows.MessageBox.Show("해당 일러스트를 내려 받으시겠습니까?", "일러스트 다운로드", MessageBoxButton.YesNo);
+                    if (res == MessageBoxResult.Yes)
+                    {
+                        using (var sv = new SaveFileDialog())
+                        {
+                            sv.Filter = @"일러스트 파일|*.jpg";
+                            sv.Title = "일러스트 내려받기";
+
+                            if (sv.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                            {
+                                using (var wb = new WebClient())
+                                {
+                                    wb.Encoding = Encoding.UTF8;
+
+                                    /*var filepath = sv.FileName.Replace(
+                                        Regex.Split(sv.FileName, @".jpg").First(),
+                                        Regex.Split(sv.FileName, @".jpg").First() + " - 게시자_" + author);*/
+                                    wb.DownloadFile("http://" + imgSrc + 'l', sv.FileName);
+                                    System.Windows.MessageBox.Show("다운로드 완료 했습니다." + "\n\n해당 일러스트 출처 입니다." +
+                                    "\n출처 복사는 해당 메시지 창이" + 
+                                    "\n활성화 된 상태에서 Ctrl+C 하시면 됩니다."+   
+                                    "\n\n게시자 : " + author + "\n출처 : " + url , "일러스트 다운로드 완료");
+                                    wb.Dispose();
+                                }
+                            }
+                        }
+                    }
+                };
+        }
+    }
+}
---
+++ getImg.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace nocoSaiga
+{
+    public class getImg
+    {
+        #region prop
+        public static string newXMLFile
+        {
+            get
+            {
+                return Application.StartupPath + @"\new.xml";
+            }
+        }
+        
+        #endregion
+
+        public void new_illust()
+        {
+            File.Delete(newXMLFile);
+            {
+                var sx = new XDocument();
+                var root = new XElement("root", "");
+                sx.Add(root);
+                sx.Save(newXMLFile);
+                sx = null;
+            }
+            using (var wb = new WebClient())
+            {
+                wb.Encoding = Encoding.UTF8;
+                var ng = wb.DownloadString(@"http://seiga.nicovideo.jp/rss/illust/new");
+                var doc = XDocument.Parse(ng);
+
+                foreach (var vc in doc.Descendants("item"))
+                {
+                    doc = XDocument.Load(newXMLFile);
+                    doc.Root.Add(new XElement("item",
+                        new XElement("author", "게시자 : " + vc.Element("author").Value),
+                        new XElement("img", vc.Element("link").Value.Replace(@"http://seiga.nicovideo.jp/seiga/im", @"http://lohas.nicoseiga.jp/img/") + "m"),
+                        new XElement("link", "출처 : " + vc.Element("link").Value)
+                        ));
+                    doc.Save(newXMLFile);
+                    doc = null;
+                }
+            }
+        }
+
+        public void sel_illust(string query)
+        {
+            File.Delete(newXMLFile);
+            {
+                var sx = new XDocument();
+                var root = new XElement("root", "");
+                sx.Add(root);
+                sx.Save(newXMLFile);
+                sx = null;
+            }
+            var ss = loginToNicoVide("http://seiga.nicovideo.jp/api/tagslide/data?query=" + query);
+            var docv = XDocument.Parse(ss);
+            
+            foreach (var sr in docv.Descendants("image"))
+            {
+                var doc = XDocument.Load(newXMLFile);
+                doc.Root.Add(new XElement("item",
+                    new XElement("author", "게시자 : " + sr.Element("nickname").Value),
+                    new XElement("img", @"http://lohas.nicoseiga.jp/img/" + sr.Element("id").Value + "m"),
+                    new XElement("link", "출처 : " + @"http://seiga.nicovideo.jp/seiga/im" + sr.Element("id").Value)
+                    ));
+                doc.Save(newXMLFile);
+                doc = null;
+            }
+
+        }
+
+        private string loginToNicoVide(string _url)
+        {
+            string id = "mafuyu12@daum.net";//メアド
+
+            string password = "park4394";//パスワード
+
+            Hashtable vals = new Hashtable();
+
+            vals["next_url"] = "";
+
+            vals["mail"] = id;
+
+            vals["password"] = password;
+
+            string url = "https://secure.nicovideo.jp/secure/login?site=niconico";
+
+            string param = "";
+
+            foreach (string k in vals.Keys)
+            {
+                param += String.Format("{0}={1}&", k, vals[k]);
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(param);
+
+            CookieContainer cc = new CookieContainer(); 
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+            req.Method = "POST";
+
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            req.ContentLength = data.Length;
+
+            req.CookieContainer = cc;
+
+
+            Stream reqStream = req.GetRequestStream();
+
+            reqStream.Write(data, 0, data.Length);
+
+            reqStream.Close();
+
+            WebResponse res = req.GetResponse();
+
+            Stream resStream = res.GetResponseStream();
+
+            Encoding encoder = Encoding.GetEncoding("UTF-8");
+
+            StreamReader sr = new StreamReader(resStream, encoder);
+
+            string result = sr.ReadToEnd();
+
+            sr.Close();
+
+            resStream.Close();
+
+            Console.WriteLine(result);
+
+            req = (HttpWebRequest)WebRequest.Create(_url);
+
+            req.CookieContainer = cc;
+
+            res = req.GetResponse();
+
+            resStream = res.GetResponseStream();
+
+            sr = new StreamReader(resStream, encoder);
+
+            result = sr.ReadToEnd();
+
+            sr.Close();
+
+            resStream.Close();
+
+            return result;
+
+        }
+    }
+}
